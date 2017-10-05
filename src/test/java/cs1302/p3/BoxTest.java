2f2a12aef00a7582292e8264ce9316ae122d13e7
Michael E. Cotterell <mepcotterell@gmail.com>
2017-10-05 18:14:27 -0400

    updated unit tests

---
+++ src/test/java/cs1302/p3/BoxTest.java
@@ -0,0 +1,36 @@
+package cs1302.p3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for the Box interface.
+ */
+public class BoxTest {
+
+    Box<String> box = null;
+
+    @BeforeEach
+    void init() {
+	box = new Box<String>();
+    } // init
+
+    @AfterEach
+    void tearDown() {
+	box = null;
+    } // tearDown
+
+    @Test
+    void testBox() {
+	final String TEST_STRING = "test";
+	box.set(TEST_STRING);
+	assertEquals(box.get(), TEST_STRING, "get() should return what is set(T)");
+    } // testBox
+
+} // BoxTest
+
